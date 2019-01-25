9ffebc5eaa06b5bb080c7d4ca71f8af0c60c7deb
Ravi-25 <ravi@standav.com>
2019-01-25 17:41:26 +0530

    Modified react-props repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
+import App1 from './State _Example1.jsx';
+import AppProp from './Props_Example1.jsx';
+import AppComp  from  './Comp_Example1.jsx';
+import AppState2 from './State_Exp2.jsx';
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+        <div>
+        <h1>Hello React :)</h1>
+          <h2>Hello World</h2>
+          <h3>Hello Ravi Pullagurla</h3>
+          <h1>Header</h1>
+          <h2>Content</h2>
+          <p>This is the content!!!</p>
+          <App1/>
+          <AppProp/>
+          <AppComp/>
+          <AppState2/>
+        </div>
     );
   }
 }
+
+
 export default App;
